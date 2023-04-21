67fe478b8d43e18bbc088509fc51aeeccffe6c40
Quang <quangpv.ndh@gmail.com>
2023-04-21 10:51:53 +0700

    level1.8:Write a program that takes a list of numbers as input and returns the median of the numbers.

--- src/median.js
+++ src/median.js
@@ -1,14 +1,16 @@
 function median(str){
     var strArr = str.split(' ');
-    var arrOder = strArr.sort(function(a, b){return a - b});
+    var arrOder = strArr.sort((function(a, b){return a - b}));
     var a = arrOder.length;
     if(a%2 == 0) {
-       var medianValue = (arrOder[(a/2)-1]+arrOder[(a/2)])/2;
+        var medianValue = (parseInt(arrOder[a/2-1])+parseInt(arrOder[a/2]))/2;
     }
     else {
-        var medianValue = arrOder[(a-1)/2];
+        var medianValue = parseInt(arrOder[(a-1)/2]);
     }
     return medianValue;
 }
 
 module.exports = median
+
+// str = "5 3 2 4 1 6"
--- src/median.test.js
+++ src/median.test.js
@@ -1,21 +1,21 @@
 const median = require("./median");
 test ("", () => {
     str = "5 3 2 4 1"
-    expect(median(str)).toBe("3");
+    expect(median(str)).toBe(3);
 });
 
 test ("", () => {
     str = "5 3 2 4 1 9 10"
-    expect(median(str)).toBe("4");
+    expect(median(str)).toBe(4);
 });
 
 test ("", () => {
     str = "-5 5 3 2 4 1"
-    expect(median(str)).toBe("2.5");
+    expect(median(str)).toBe(2.5);
 });
 
 
 test ("", () => {
     str = "5 3 2 4 1 6"
-    expect(median(str)).toBe("3.5");
+    expect(median(str)).toBe(3.5);
 });
