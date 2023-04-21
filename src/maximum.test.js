fb82304259bf3d06561b41a1bad8b6add79733dd
Quang <quangpv.ndh@gmail.com>
2023-04-21 15:08:49 +0700

    Sửa tên hàm

--- src/maximum.test.js
+++ src/maximum.test.js
@@ -1,23 +1,23 @@
-const maximum = require("./maximum");
+const maximumSumOfContiguousSubarryy = require("./maximum");
 
 test("", () => {
     var arr = [-2, -3, 4, -1, -2, 1, 5, -3]
-  expect(maximum(arr)).toBe(7);
+  expect(maximumSumOfContiguousSubarryy(arr)).toBe(7);
 });
 
 
 test("", () => {
     var arr = [-2, -3, 4, -1, -2, 1, 5, -3, 10]
-  expect(maximum(arr)).toBe(14);
+  expect(maximumSumOfContiguousSubarryy(arr)).toBe(14);
 });
 
 test("", () => {
     var arr = [-2, -3, 4, -1, -2, 1, 5, -3, 10, -5]
-  expect(maximum(arr)).toBe(14);
+  expect(maximumSumOfContiguousSubarryy(arr)).toBe(14);
 });
 
 
 test("", () => {
     var arr = [-2, -3, 4, -1, -2, 1, 5, -3, 10, -5, 20]
-  expect(maximum(arr)).toBe(29);
+  expect(maximumSumOfContiguousSubarryy(arr)).toBe(29);
 });
