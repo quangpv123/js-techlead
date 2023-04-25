aa67c7e631f8f70f2cc33d4b202b8fdc52fe3aa9
Quang <quangpv.ndh@gmail.com>
2023-04-25 09:54:29 +0700

    level3.4: Write a program that takes a list of strings as input and returns the two strings with the largest overlap of characters.

--- src/level34.js
+++ src/level34.js
@@ -5,22 +5,20 @@ function twoStringLargestOverlapOfCharacters(arr){
         for(j=i+1; j<arr.length; j++){
             var overlap = 0;
             for(k=0; k<arr[i].length; k++){
-                var overlap = 0;
+                // var overlap = 0;
                if (arr[j].includes(arr[i][k])){
                     overlap++;
-               }
-
-               if(overlap > maxOverlap){
+               }                
+            }
+            if(overlap > maxOverlap){
                 maxOverlap = overlap;
                 twoStringMaxOverlap = [arr[i], arr[j]];
-            }
-                
             }
             
         }
 
     }
-    return twoStringMaxOverlap;
+    return twoStringMaxOverlap.join(', ');
 }
 
 module.exports = twoStringLargestOverlapOfCharacters
--- src/level34.test.js
+++ src/level34.test.js
@@ -2,11 +2,27 @@ const twoStringLargestOverlapOfCharacters = require("./level34");
 
 test("", () => {
     var arr = ["abcdefg", "cdefghijklm", "ghijklmnopqr"];
-    expect(twoStringLargestOverlapOfCharacters(arr)).toBe("abcdefg, cdefghijklm");
+    expect(twoStringLargestOverlapOfCharacters(arr)).toBe("cdefghijklm, ghijklmnopqr");
 });
 
 
 test("", () => {
     var arr = ["hello", "world", "foobar", "barfoo", "he", "llo"];
     expect(twoStringLargestOverlapOfCharacters(arr)).toBe("foobar, barfoo");
+});
+
+
+test("", () => {
+    var arr = ["xinchao", "world", "foobar", "chaoxin", "he", "llo"];
+    expect(twoStringLargestOverlapOfCharacters(arr)).toBe("xinchao, chaoxin");
+});
+
+test("", () => {
+    var arr = ["helloworld", "world", "foobar", "barfoo", "he", "llo"];
+    expect(twoStringLargestOverlapOfCharacters(arr)).toBe("helloworld, world");
+});
+
+test("", () => {
+    var arr = ["helloworld", "world", "foobarhelloworld", "barfoo", "he", "llo"];
+    expect(twoStringLargestOverlapOfCharacters(arr)).toBe("helloworld, foobarhelloworld");
 });
