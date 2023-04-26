f6a963a0a567fbf92ee706c5367da18d3c5a283d
Quang <quangpv.ndh@gmail.com>
2023-04-26 16:45:48 +0700

    chưa làm được

---
+++ src/level35.js
@@ -0,0 +1,11 @@
+function smallestPositiveInteger(arr){
+    var sums = [];
+    for(i=0; i < arr.lenght; i++){                 
+        var sum = arr[i];
+        for(j=i+1; j < arr.length; i++){
+            sum += arr[j];
+            if()
+        }
+
+    }
+}
